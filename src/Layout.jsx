76a96fc220c429ca5d0c1039aa5b856b0a5dd58b
msyed <msyed@gmai.com>
2025-04-19 15:32:19 +0500

    commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from './Customer_dashboard/header'
 import { Outlet } from 'react-router-dom'
 import BGComponent from './Customer_dashboard/BGComponent'
 import Footer from './Customer_dashboard/Footer'
